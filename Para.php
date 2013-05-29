d11943c4e9b5171c34478902b6ef379e9fdde6d6
Alexandre Anastassov <a.anastassov@iaea.org>
2013-05-29 18:19:39 +0200

    working version

--- Para.php
+++ Para.php
@@ -33,10 +33,53 @@ class Para {
             if (is_int($columns) && $columns > 0) {
                 $this->_columns = $columns;
             } else {
-                throw new InvalidArgumentException('columns method only excepts positive integers. Input was: '.$columns);
+                throw new InvalidArgumentException('columns method only excepts positive integers. Input was: ' . $columns);
             }
         }
         return $this->_columns;
+    }
+
+    public function format_for_web($text) {
+        $text = str_replace("\n", "<br/>", $text);
+        return str_replace("<br/><br/>", "<p/>", $text);
+    }
+
+    public function format($text) {
+        $lines = explode("\n\n", $text);
+        $result = array_map(function($text) {return $this->format_para($text);}, $lines);
+        return implode("\n\n", $result);
+    }
+
+    private function format_para($text) {
+        $para = '';
+        $columns = $this->_columns;
+        $cols_left = $columns;
+        preg_match_all('/(\S+)/', $text, $matches);
+
+        $words = array();
+        foreach($matches[0] as $word) $words[] = $word;
+        
+        while (($word = array_shift($words))) {
+            $word_length = strlen($word);
+            if ($cols_left > $word_length) {
+                if ($cols_left < $columns)
+                    $para .= ' ';
+                $para .= $word;
+                $cols_left -= $word_length;
+            }
+            elseif ($word_length <= $columns) {
+                $para .= "\n$word";
+                $cols_left = $columns - $word_length;
+            } else {
+                $l = $columns - 1;
+                $part = substr($word, 0, $l);
+                $word = substr($word, $l, strlen($word) - $l);
+                $para .= "\n$part-\n";
+                $cols_left = $columns;
+                array_unshift($words, $word);
+            }
+        }
+        return $para;
     }
 
 }
--- index.php
+++ index.php
@@ -1,13 +1,65 @@
-<?php
-include_once "Para.php";
+<html>
+    <head>
+        <title>TDD Demo</title>
+        <style>
+            body { 
+                font-family: Arial, Helvetica, sans-serif;
+                font-size: 1.2em;
+                color: grey;
+            }
 
-$para = new Para();
-$para->columns(12);
-echo $para->columns();
+            /* 
+            Flush left, ragged right
+            1.25em basic leading (line height), one lead boundary (vertical margins)
+            */
 
-echo "\n\n********************************************************************\n";
-echo "dirname(__FILE__) " . dirname(__FILE__) . "\n";
-echo "dirname(dirname(__FILE__)) " . dirname(dirname(__FILE__)) . "\n";
-echo "basename(dirname(dirname(__FILE__))) " . basename(dirname(dirname(__FILE__))) . "\n";
+            p {
+                line-height: 1.25em;
+                margin: 1.25em 0;
+                text-align: left;
+            }
 
-?>
+            form {
+                color: darkgrey;
+            }
+        </style>
+    </head>
+    <body>
+        <?php
+        include_once "Para.php";
+        if (!isset($default_value)) {
+            $default_value = Para::DEFAULT_COLUMNS;
+        }
+        ?>
+
+        <!-- form -->
+        <form action="index.php" method="post">
+            Number of columns: <input type="text" name="columns" maxlength="4">
+            <input type="submit" name="formSubmit" value="Submit">
+        </form>
+
+        <?php
+        if (key_exists('columns', $_POST) && $_POST['formSubmit'] == "Submit") {
+            $columns = (int) $_POST['columns'];
+        } else {
+            $columns = Para::DEFAULT_COLUMNS;
+        }
+        if (is_int($columns) && $columns > 1) {
+            processForm($columns);
+        }
+        $default_value = $columns;
+
+        function processForm($columns) {
+            $para = new Para();
+            $para->columns($columns);
+            $text = <<<EOF
+My money's in that office, right? If she start giving me some bullshit about it ain't there, and we got to go someplace else and get it, I'm gonna shoot you in the head then and there. Then I'm gonna shoot that bitch in the kneecaps, find out where my goddamn money is. She gonna tell me too. Hey, look at me when I'm talking to you, motherfucker. You listen: we go in there, and that nigga Winston or anybody else is in there, you the first motherfucker to get shot. You understand?
+             
+Normally, both your asses would be dead as fucking fried chicken, but you happen to pull this shit while I'm in a transitional period so I don't wanna kill you, I wanna help you. But I can't give you this case, it don't belong to me. Besides, I've already been through too much shit this morning over this case to hand it over to your dumb ass.
+EOF;
+            echo "[Lenght: $columns]<br/>";
+            echo $para->format_for_web($text);
+        }
+        ?>
+    </body>
+</html>
